79371ff73e8039cc31f55783ec7e5f50a11d6abf
Jesus Montoya <chumontoya@hotmail.com>
2024-07-17 16:37:58 -0400

    Ultimo commit

--- 01-vnc-subnets/network.tf
+++ 01-vnc-subnets/network.tf
@@ -5,3 +5,12 @@ resource "oci_core_virtual_network" "VCN_produccion" { # definir el recurso de l
   compartment_id = oci_identity_compartment.produccion # definir el OCID del compartimento
   display_name   = "vcnproduccion" # definir el nombre de la VCN
 }
+
+# Subnet
+resource "oci_core_subnet" "WebSubnet" { # definir el recurso de la subred
+  cidr_block        = var.Subnet-CIDR # definir el bloque CIDR de la subred
+  display_name      = "WebSubnet" # definir el nombre de la subred
+  dns_label         = "WebSubnetN1"   # definir la etiqueta DNS de la subred
+  compartment_id    = oci_identity_compartment.produccion # definir el OCID del compartimento
+  vcn_id            = oci_core_virtual_network.VCN_produccion # definir el OCID de la VCN
+}
