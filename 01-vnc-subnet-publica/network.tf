0347b99eec9293cfd252cc53d2312666757a6ecc
Jesus Montoya <chumontoya@hotmail.com>
2024-07-17 19:50:15 -0400

    Ultimo commit

--- 01-vnc-subnet-publica/network.tf
+++ 01-vnc-subnet-publica/network.tf
@@ -2,7 +2,7 @@
 resource "oci_core_virtual_network" "VCN_produccion" { # definir el recurso de la red virtual (VCN)
   cidr_block     = var.VCN-CIDR # definir el bloque CIDR de la VCN
   dns_label      = "vcnproduccion" # definir la etiqueta DNS de la VCN
-  compartment_id = oci_identity_compartment.produccion # definir el OCID del compartimento
+  compartment_id = oci_identity_compartment.produccion.id # definir el OCID del compartimento
   display_name   = "vcnproduccion" # definir el nombre de la VCN
 }
 
